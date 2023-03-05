6df8ab621b56c54375762909f973fc5805f33824
o080o <o080o@tutanota.com>
2023-03-04 17:50:28 -0700

    try... that?

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const withTM = require('next-transpile-modules')([
+]);
+
+
+module.exports = withTM({
+  swcMinify: false,
+  trailingSlash: true, //automatically redirect without a trailing slash. this lets us use query params...
+  env: {
+    // HOST
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      }
+    ]
+  }
+});
+
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+// module.exports = withBundleAnalyzer({})
